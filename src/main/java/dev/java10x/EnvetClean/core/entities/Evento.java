041394dd421b5b192f28cc873122f0db7674410a
Alan Miranda <alannub6@gmail.com>
2025-01-22 09:52:31 -0300

    feat(core): add event entity and use cases with specific event types enum

---
+++ src/main/java/dev/java10x/EnvetClean/core/entities/Evento.java
@@ -0,0 +1,18 @@
+package dev.java10x.EnvetClean.core.entities;
+
+import dev.java10x.EnvetClean.core.enums.TipoEvento;
+
+import java.time.LocalDateTime;
+
+public record Evento(
+        Long id,
+        String nome,
+        String descricao,
+        String identificador,
+        LocalDateTime dataInicio,
+        LocalDateTime dataFim,
+        String local,
+        String organizador,
+        TipoEvento tipo
+) {
+}
